3e850e2a76d2f6a43749ef632974770de356b904
shahar-trullion <87264682+shahar-trullion@users.noreply.github.com>
2023-02-14 17:19:28 +0200

    update setoupput

--- index.js
+++ index.js
@@ -6,76 +6,100 @@ const github = require('@actions/github');
 const request = require('request-promise-native');
 const dateFormat = require('dateformat');
 const token = require('@highwaythree/jira-github-actions-common');
+
+const os = require("os");
+const fs = require("fs");
+
+function setOutput(key, value) {
+  // Temporary hack until core actions library catches up with github new recommendations
+  const output = process.env["GITHUB_OUTPUT"];
+  fs.appendFileSync(output, `${key}=${value}${os.EOL}`);
+}
+
 async function submitDeploymentInfo(accessToken) {
-    const cloudInstanceBaseUrl = core.getInput('cloud-instance-base-url');
-    const cloudURL = new url_1.URL('/_edge/tenant_info', cloudInstanceBaseUrl);
-    let cloudId = await request(cloudURL.href);
-    cloudId = JSON.parse(cloudId);
-    cloudId = cloudId.cloudId;
-    const deploymentSequenceNumber = core.getInput('deployment-sequence-number');
-    const updateSequenceNumber = core.getInput('update-sequence-number');
-    const issueKeys = core.getInput('issue-keys');
-    const displayName = core.getInput('display-name');
-    const url = core.getInput('url');
-    const description = core.getInput('description');
-    let lastUpdated = core.getInput('last-updated');
-    const label = core.getInput('label');
-    const state = core.getInput('state');
-    const pipelineId = core.getInput('pipeline-id');
-    const pipelineDisplayName = core.getInput('pipeline-display-name');
-    const pipelineUrl = core.getInput('pipeline-url');
-    const environmentId = core.getInput('environment-id');
-    const environmentDisplayName = core.getInput('environment-display-name');
-    const environmentType = core.getInput('environment-type');
-    // console.log("lastUpdated: " + lastUpdated);
-    lastUpdated = dateFormat(lastUpdated, "yyyy-mm-dd'T'HH:MM:ss'Z'");
-    const deployment = {
-        schemaVersion: "1.0",
-        deploymentSequenceNumber: deploymentSequenceNumber || process.env['GITHUB_RUN_ID'],
-        updateSequenceNumber: updateSequenceNumber || process.env['GITHUB_RUN_ID'],
-        issueKeys: issueKeys.split(',') || [],
-        displayName: displayName || '',
-        url: url || `${github.context.payload.repository.url}/actions/runs/${process.env['GITHUB_RUN_ID']}`,
-        description: description || '',
-        lastUpdated: lastUpdated || '',
-        label: label || '',
-        state: state || '',
-        pipeline: {
-            id: pipelineId || `${github.context.payload.repository.full_name} ${github.context.workflow}`,
-            displayName: pipelineDisplayName || `Workflow: ${github.context.workflow} (#${process.env['GITHUB_RUN_NUMBER']})`,
-            url: pipelineUrl || `${github.context.payload.repository.url}/actions/runs/${process.env['GITHUB_RUN_ID']}`,
-        },
-        environment: {
-            id: environmentId || '',
-            displayName: environmentDisplayName || '',
-            type: environmentType || '',
-        }
-    };
-    let bodyData = {
-        deployments: [deployment],
-    };
-    bodyData = JSON.stringify(bodyData);
-    const options = {
-        method: 'POST',
-        url: "https://api.atlassian.com/jira/deployments/0.1/cloud/" + cloudId + "/bulk",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: "Bearer " + accessToken,
-        },
-        body: bodyData,
-    };
-    let responseJson = await request(options);
-    let response = JSON.parse(responseJson);
-    if (response.rejectedDeployments && response.rejectedDeployments.length > 0) {
-        const rejectedDeployment = response.rejectedDeployments[0];
-        // console.log("errors: ", rejectedDeployment.errors);
-        let errors = rejectedDeployment.errors.map((error) => error.message).join(',');
-        errors.substr(0, errors.length - 1);
-        // console.log("joined errors: ", errors);
-        core.setFailed(errors);
-    }
-    core.setOutput("response", responseJson);
+  const cloudInstanceBaseUrl = core.getInput("cloud-instance-base-url");
+  const cloudURL = new url_1.URL("/_edge/tenant_info", cloudInstanceBaseUrl);
+  let cloudId = await request(cloudURL.href);
+  cloudId = JSON.parse(cloudId);
+  cloudId = cloudId.cloudId;
+  const deploymentSequenceNumber = core.getInput("deployment-sequence-number");
+  const updateSequenceNumber = core.getInput("update-sequence-number");
+  const issueKeys = core.getInput("issue-keys");
+  const displayName = core.getInput("display-name");
+  const url = core.getInput("url");
+  const description = core.getInput("description");
+  let lastUpdated = core.getInput("last-updated");
+  const label = core.getInput("label");
+  const state = core.getInput("state");
+  const pipelineId = core.getInput("pipeline-id");
+  const pipelineDisplayName = core.getInput("pipeline-display-name");
+  const pipelineUrl = core.getInput("pipeline-url");
+  const environmentId = core.getInput("environment-id");
+  const environmentDisplayName = core.getInput("environment-display-name");
+  const environmentType = core.getInput("environment-type");
+  // console.log("lastUpdated: " + lastUpdated);
+  lastUpdated = dateFormat(lastUpdated, "yyyy-mm-dd'T'HH:MM:ss'Z'");
+  const deployment = {
+    schemaVersion: "1.0",
+    deploymentSequenceNumber:
+      deploymentSequenceNumber || process.env["GITHUB_RUN_ID"],
+    updateSequenceNumber: updateSequenceNumber || process.env["GITHUB_RUN_ID"],
+    issueKeys: issueKeys.split(",") || [],
+    displayName: displayName || "",
+    url:
+      url ||
+      `${github.context.payload.repository.url}/actions/runs/${process.env["GITHUB_RUN_ID"]}`,
+    description: description || "",
+    lastUpdated: lastUpdated || "",
+    label: label || "",
+    state: state || "",
+    pipeline: {
+      id:
+        pipelineId ||
+        `${github.context.payload.repository.full_name} ${github.context.workflow}`,
+      displayName:
+        pipelineDisplayName ||
+        `Workflow: ${github.context.workflow} (#${process.env["GITHUB_RUN_NUMBER"]})`,
+      url:
+        pipelineUrl ||
+        `${github.context.payload.repository.url}/actions/runs/${process.env["GITHUB_RUN_ID"]}`,
+    },
+    environment: {
+      id: environmentId || "",
+      displayName: environmentDisplayName || "",
+      type: environmentType || "",
+    },
+  };
+  let bodyData = {
+    deployments: [deployment],
+  };
+  bodyData = JSON.stringify(bodyData);
+  const options = {
+    method: "POST",
+    url:
+      "https://api.atlassian.com/jira/deployments/0.1/cloud/" +
+      cloudId +
+      "/bulk",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessToken,
+    },
+    body: bodyData,
+  };
+  let responseJson = await request(options);
+  let response = JSON.parse(responseJson);
+  if (response.rejectedDeployments && response.rejectedDeployments.length > 0) {
+    const rejectedDeployment = response.rejectedDeployments[0];
+    // console.log("errors: ", rejectedDeployment.errors);
+    let errors = rejectedDeployment.errors
+      .map((error) => error.message)
+      .join(",");
+    errors.substr(0, errors.length - 1);
+    // console.log("joined errors: ", errors);
+    core.setFailed(errors);
+  }
+  setOutput("response", responseJson);
 }
 exports.submitDeploymentInfo = submitDeploymentInfo;
 (async function () {
